9e47fa167207672bbcbd4ef48a34addabcf558b8
realdoc123 <padilamyeshwant17@gmail.com>
2024-04-16 16:50:21 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name='ML-Project',
+    version='0.0.1',
+    author='padil',
+    author_email='padilamyeshwant17@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'seaborn'  ]
+)
